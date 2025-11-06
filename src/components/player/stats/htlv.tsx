e38c7858f27f2bdb8bac34e90dbdb34a8a4ab20a
Julien Constant <julienconstant190@gmail.com>
2025-11-06 11:59:24 +0100

    ref: re-add delta variation to kd & hltv rating

--- src/components/player/stats/htlv.tsx
+++ src/components/player/stats/htlv.tsx
@@ -6,32 +6,71 @@ import 'chart.js/auto';
 import './grid.css';
 
 export default function HLTVStats() {
-	const { user: { stats } } = usePlayerData();
+	const { user: { stats }, loading } = usePlayerData();
 
-	const offset = 0.4;
-	const maxRating = 1.8;
+	const [HLTVDelta, setHLTVDelta] = useState(0);
+	const [HLTVDeltaStatus, setHLTVDeltaStatus] = useState<'up' | 'same' | 'down'>('same');
+	
+	const { baseRating, rating1, rating2, isMaxed, isOverMaxed } = useMemo(() => {
+		const offset = 0.4;
+		const maxHLTVRating = 1.8;
+		const baseRating = stats?.overall?.rating ?? 0;
 
-	const baseRating = stats?.overall?.rating ?? 0;
+		let rating1 = baseRating - offset;
+		if (rating1 > maxHLTVRating) rating1 = maxHLTVRating;
 
-	let rating1 = baseRating - offset;
-	if (rating1 > maxRating) rating1 = maxRating;
+		rating1 = (rating1 / (maxHLTVRating / 2)) * 100; // normalize
+		let rating2 = 100 - rating1;
 
-	rating1 = (rating1 / (maxRating / 2)) * 100; // normalize
-	let rating2 = 100 - rating1;
+		let isMaxed = false;
+		if (baseRating > ((maxHLTVRating / 2) + offset)) {
+			isMaxed = true;
+			rating1 = rating1 - 100;
+			rating2 = 100 - rating1;
+		}
 
-	let isMaxed = false;
-	if (baseRating > ((maxRating / 2) + offset)) {
-		isMaxed = true;
-		rating1 = rating1 - 100;
-		rating2 = 100 - rating1;
-	}
+		const isOverMaxed = baseRating > maxHLTVRating;
 
-	let isOverMaxed = false;
-	if (baseRating > maxRating) {
-		isOverMaxed = true;
-	}
+		return {
+			baseRating,
+			rating1,
+			rating2,
+			isMaxed,
+			isOverMaxed
+		};
+	}, [stats?.overall?.rating]);
 
-	// TODO: re-add the up/down arrow indicators for rating changes
+	useEffect(() => {
+		if (loading) return;
+
+		const topContainer = document.getElementById('rating-delta');
+		const container    = topContainer && topContainer.querySelector('span');
+		const element      = container    && container.querySelector('span');
+
+		if (!element) return;
+
+		const className = element.className
+			.split(' ')
+			.filter((el) => el.startsWith('stat-'))[0] as `stat-${string}`;
+
+		switch (className) {
+			case 'stat-up':
+				setHLTVDeltaStatus('up');
+				break;
+			case 'stat-same':
+				setHLTVDeltaStatus('same');
+				setHLTVDelta(0);
+				break;
+			case 'stat-down':
+				setHLTVDeltaStatus('down');
+				break;
+		}
+
+		if (className !== 'stat-same') {
+			setHLTVDelta(Math.abs(parseFloat(element.getAttribute('data-delta') || '0')));
+		}
+
+	}, [loading]);
 
 	return (
 		<Tile 
@@ -44,8 +83,14 @@ export default function HLTVStats() {
 							HLTV RATING
 						</span>
 					</div>
-					<div className="text-over-chart">
+					<div className="text-over-chart col center-y center-x">
 						{baseRating}
+						<div className="row text">
+							{HLTVDeltaStatus === 'up'   && (<span>▲</span>)}
+							{HLTVDeltaStatus === 'same' && (<span>No variation</span>)}
+							{HLTVDeltaStatus === 'down' && (<span>▼</span>)}
+							{HLTVDelta !== 0 && (<span>{HLTVDelta}</span>)}
+						</div>
 					</div>
 					<Chart
 						type="doughnut"
--- src/components/player/stats/kd.tsx
+++ src/components/player/stats/kd.tsx
@@ -6,32 +6,66 @@ import 'chart.js/auto';
 import './grid.css';
 
 export default function KDStats() {
-	const { user: { stats } } = usePlayerData();
+	const { user: { stats }, loading } = usePlayerData();
 
-	const maxKpd = 3.0;
-	const baseKpd = stats?.overall?.kpd ?? 0;
+	const [kpdDelta, setKpdDelta] = useState(0);
+	const [kpdDeltaStatus, setKpdDeltaStatus] = useState<'up' | 'same' | 'down'>('same');
+	
+	const { baseKpd, kpd1, kpd2, isMaxed, isOverMaxed } = useMemo(() => {
+		const maxKpd = 3.0;
+		const baseKpd = stats?.overall?.kpd ?? 0;
 
-	let kpd1 = baseKpd;
-	if (kpd1 > maxKpd) kpd1 = maxKpd;
+		const isOverMaxed = baseKpd > maxKpd;
+		const isMaxed = baseKpd > (maxKpd / 2);
 
-	kpd1 = (kpd1 / (maxKpd / 2)) * 100; // normalize
+		let kpd1 = 0;
+		let kpd2 = 0;
 
-	let kpd2 = 100 - kpd1;
-	if (kpd2 < 0) kpd2 = 0;
+		if (isOverMaxed) {
+			kpd1 = 100;
+			kpd2 = 0;
+		} else if (isMaxed) {
+			kpd1 = ((baseKpd - (maxKpd / 2)) / (maxKpd / 2)) * 100;
+			kpd2 = 100 - kpd1;
+		} else {
+			kpd1 = (baseKpd / (maxKpd / 2)) * 100;
+			kpd2 = 100 - kpd1;
+		}
 
-	let isMaxed = false;
-	if (baseKpd > (maxKpd / 2)) {
-		isMaxed = true;
-		kpd1 = kpd1 - 100;
-		kpd2 = 100 - kpd1;
-	}
+		return { baseKpd, kpd1, kpd2, isMaxed, isOverMaxed };
+	}, [stats?.overall?.kpd]);
 
-	let isOverMaxed = false;
-	if (baseKpd > maxKpd) {
-		isOverMaxed = true;
-	}
+	useEffect(() => {
+		if (loading) return;
 
-	// TODO: re-add the up/down arrow indicators for kd changes
+		const topContainer = document.getElementById('kpd-delta');
+		const container    = topContainer && topContainer.querySelector('span');
+		const element      = container    && container.querySelector('span');
+
+		if (!element) return;
+
+		const className = element.className
+			.split(' ')
+			.filter((el) => el.startsWith('stat-'))[0] as `stat-${string}`;
+
+		switch (className) {
+			case 'stat-up':
+				setKpdDeltaStatus('up');
+				break;
+			case 'stat-same':
+				setKpdDeltaStatus('same');
+				setKpdDelta(0);
+				break;
+			case 'stat-down':
+				setKpdDeltaStatus('down');
+				break;
+		}
+
+		if (className !== 'stat-same') {
+			setKpdDelta(Math.abs(parseFloat(element.getAttribute('data-delta') || '0')));
+		}
+
+	}, [loading]);
 
 	return (
 		<Tile 
@@ -45,8 +79,14 @@ export default function KDStats() {
 							K/D
 						</span>
 					</div>
-					<div className="text-over-chart">
+					<div className="text-over-chart col center-y center-x">
 						{baseKpd}
+						<div className="row text">
+							{kpdDeltaStatus === 'up'   && (<span>▲</span>)}
+							{kpdDeltaStatus === 'same' && (<span>No variation</span>)}
+							{kpdDeltaStatus === 'down' && (<span>▼</span>)}
+							{kpdDelta !== 0 && (<span>{kpdDelta}</span>)}
+						</div>
 					</div>
 					<Chart
 						type="doughnut"
