4619347dda93436b3963a328f74083bbaa6e6935
Julien <julienconstant190@gmail.com>
2026-07-30 21:25:42 +0200

    deps: bump the dependencies & add web-ext

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "wxt";
+import os from "node:os";
 
 const CHROMIUM_ARGS: string[] = [
 	// Edge says it's not working anymore but it seems to still mitigate CloudFlare issues
 	"--disable-blink-features=AutomationControlled",
 ];
+
+const EDGE_BIN_PATH = (() => {
+	switch (os.platform()) {
+		case "win32":
+			return "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
+		case "darwin":
+			return "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge";
+		default:
+			return undefined;
+	}
+})();
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -14,6 +26,7 @@ export default defineConfig({
 		startUrls: ["https://csstats.gg/player/76561198088629896"],
 		chromiumArgs: CHROMIUM_ARGS,
 		keepProfileChanges: true,
+		...(EDGE_BIN_PATH ? { binaries: { edge: EDGE_BIN_PATH } } : {}),
 	},
 	manifest: {
 		name: "CSStats+",
